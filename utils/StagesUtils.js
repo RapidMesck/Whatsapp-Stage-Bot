196ddee5ed69f698b46fef8642270be327f5ac52
RapidMesck <44342800+RapidMesck@users.noreply.github.com>
2023-12-24 11:51:43 -0300

    Update StagesUtils.js

--- utils/StagesUtils.js
+++ utils/StagesUtils.js
@@ -1,4 +1,4 @@
-const { checkInsertedUser, updateUserStatus, getUserStatus, updateUserLastMessage } = require('../utils/DatabaseUtils.js')
+const { insertUser, checkInsertedUser, updateUserStatus, getUserStatus, updateUserLastMessage } = require('../utils/DatabaseUtils.js')
 
 const Controller = async(msg) => {
     try{
@@ -7,10 +7,9 @@ const Controller = async(msg) => {
         const checkUser = await checkInsertedUser(userId);
 
         if(!checkUser){
-            insertUser(msg.from).then(async() => {
-                await updateUserStatus(userId, '1');
-                return "Ola, este e o atendimento automatico Digite\n 1 - para uma resposta\n4 - Para ser transferido para o stagio 2";
-            })
+            await insertUser(msg.from);
+            await updateUserStatus(userId, '1');
+            return "Ola, este e o atendimento automatico Digite\n 1 - para uma resposta\n4 - Para ser transferido para o stagio 2";
         }else{
             await updateUserLastMessage(userId, msg.body);
             const userStage = await getUserStatus(userId);
@@ -18,14 +17,14 @@ const Controller = async(msg) => {
                 await updateUserStatus(userId, '1');
                 return "Ola, este e o atendimento automatico Digite\n 1 - para uma resposta\n4 - Para ser transferido para o stagio 2";
             }else{
-                let { result } = require(`../stages/stage${userStage}.js`);
+                const { result } = require(`../stages/stage${userStage}.js`);
                 const respose = await result(userId, msg);
                 return respose;
             }
         }
     }catch(err){
         console.error(err);
-        return null
+        return null;
     }
 }
 
